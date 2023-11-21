7e82958ed3682702c5b0d83b9881d2d8d341399f
eugenio socha <eugenio.socha2004@gmail.com>
2023-11-21 00:38:26 -0400

    Criado o arquivo index.js e feita algumas estilizações

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function enviar(){
+ 
+    let nome = document.getElementById("nome").value;
+    // let telefone = document.getElementById("txttelefone").value;
+    // let email = document.getElementById("txtemail").value;
+     
+    // let texto = "Nome: " +nome + "; Telefone: "+ telefone + "; Email: "+email; // como quiser formatar
+    window.alert(`Nome: ${nome}`)
+    return true;
+     
+    }
